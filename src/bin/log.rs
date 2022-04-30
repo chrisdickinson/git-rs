a55eae3665f4c3d5975e43bf4a2a4e8b3da3659b
Chris Dickinson <chris@neversaw.us>
2022-04-29 23:42:25 -0700

    Add clap for arg parsing; add cwd flag to git_rs_log

--- src/bin/log.rs
+++ src/bin/log.rs
@@ -1,21 +1,27 @@
 extern crate git_rs;
 
-use git_rs::stores::{fs as gitfs};
+use git_rs::stores::fs as gitfs;
 use git_rs::refs::RefSet;
+use std::path::PathBuf;
+use clap::Parser;
+
+#[derive(Parser)]
+struct Args {
+    #[clap(long)]
+    cwd: Option<PathBuf>,
+    branch: Option<String>,
+}
 
 pub fn main() -> std::io::Result<()> {
-    let current_dir = std::env::current_dir()?;
+    let args = Args::parse();
+
+    let current_dir = args.cwd.or_else(|| std::env::current_dir().ok()).unwrap();
     let storage_set = gitfs::from(current_dir.as_path())?;
     let ref_set = RefSet::from_path(current_dir.as_path())?;
-    let args: Vec<String> = std::env::args().collect();
 
-    let query = if args.len() < 2 {
-        "HEAD"
-    } else {
-        &args[1]
-    };
+    let query = args.branch.unwrap_or_else(|| "HEAD".to_string());
 
-    let id = match ref_set.deref(query) {
+    let id = match ref_set.deref(&query) {
         Some(result) => result.clone(),
         None => {
             match query.parse() {
@@ -27,8 +33,10 @@ pub fn main() -> std::io::Result<()> {
 
     for (id, commit) in storage_set.commits(&id, None) {
         let message = std::str::from_utf8(commit.message()).expect("not utf8");
-        let lines: Vec<&str> = message.split('\n').collect();
-        println!("\x1b[33m{} \x1b[0m{}", id, lines[0]);
+
+        let idx = message.find('\n').unwrap_or(message.len());
+
+        println!("\x1b[33m{} \x1b[0m{}", id, &message[0..idx]);
     };
 
     Ok(())
